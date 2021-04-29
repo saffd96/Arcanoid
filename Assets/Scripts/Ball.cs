ca2a5585ed8a869eb1f498debb93a87b07ea78d6
Saffd <saffd@tut.by>
2021-04-29 23:49:08 +0300

    added sprites

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 direction;
+
+    private bool isStarted;
+
+    private void Update()
+    {
+        if (isStarted && Input.GetMouseButtonDown(0))
+        {
+            StartBall();
+        }
+    }
+
+    private void StartBall()
+    {
+        Vector2 force = new Vector2(1, 1) * speed;
+        rb.AddForce(force);
+        isStarted = true;
+    }
+
+}
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    #region Unity Lifecycle
+
+    private void Update()
+    {
+        Vector3 positionInPixels = Input.mousePosition;
+
+        Vector3 positionInWorld = Camera.main.ScreenToWorldPoint(positionInPixels);
+
+
+        Vector3 padPosition = positionInWorld;
+
+        padPosition.y = transform.position.y;
+
+        transform.position = padPosition;
+
+        Debug.Log($"{padPosition}");
+
+    }
+
+    #endregion
+}
